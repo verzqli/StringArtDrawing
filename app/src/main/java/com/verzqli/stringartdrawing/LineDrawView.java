329df95836205eee129c9c3d59698e08eb707475
xupei <710275215@qq.com>
2019-11-30 18:25:20 +0800

    init

---
+++ app/src/main/java/com/verzqli/stringartdrawing/LineDrawView.java
@@ -0,0 +1,72 @@
+package com.verzqli.stringartdrawing;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class LineDrawView extends View {
+    private Bitmap cacheBitmap = null;
+    private Canvas cacheCanvas = null;
+    private int width;
+    private int height;
+    private Paint paint;
+    private Paint bitmapPaint;
+    private int count = 150;
+    private ArrayList linePoint = new ArrayList(count);
+
+    public LineDrawView(Context context) {
+        this(context, null);
+    }
+
+    public LineDrawView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+        paint = new Paint();
+        bitmapPaint = new Paint();
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+        cacheBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        cacheCanvas = new Canvas();
+        cacheCanvas.setBitmap(cacheBitmap);
+        int perimeter = (width + height) << 1;
+        int distance = perimeter / count;
+        for (int i = 0; i < count; i++) {
+            int result = i * distance;
+            if (result >= 0 && result < width) {
+                linePoint.add(new Point(result, 0));
+            } else if (result >= width && result < (width + height)) {
+                linePoint.add(new Point(width, result - width));
+            } else if (result >= (width + height) && result < (width * 2 + height)) {
+                linePoint.add(new Point(width - (result - (width + height)), height));
+            } else {
+                linePoint.add(new Point(0, height - (result - (width * 2 + height))));
+            }
+        }
+    }
+
+    public LineDrawView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(cacheBitmap, 0, 0, bitmapPaint);
+    }
+
+    public void setLine(int x, int y) {
+
+    }
+}
---
+++ app/src/main/java/com/verzqli/stringartdrawing/MainActivity.java
@@ -0,0 +1,121 @@
+package com.verzqli.stringartdrawing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("HandlerLeak")
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button rgb2greyBtn = findViewById(R.id.rgb2greybtn);
+        ImageView imageView1 = findViewById(R.id.imageView1);
+        final ImageView imageView2 = findViewById(R.id.imageView2);
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+        imageView1.setImageBitmap(bitmap);
+        rgb2greyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                imageView2.setImageBitmap(convertGreyImg(bitmap));
+            }
+        });
+    }
+
+    /**
+     * 将彩色图转换为灰度图
+     *
+     * @param img 位图
+     * @return 返回转换好的位图
+     */
+    public Bitmap convertGreyImg(Bitmap img) {
+        int width = img.getWidth();         //获取位图的宽
+        int height = img.getHeight();       //获取位图的高
+
+        int[] pixels = new int[width * height]; //通过位图的大小创建像素点数组
+
+        img.getPixels(pixels, 0, width, 0, 0, width, height);
+        int alpha = 0xFF << 24;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int grey = pixels[width * i + j];
+
+                int red = ((grey & 0x00FF0000) >> 16);
+                int green = ((grey & 0x0000FF00) >> 8);
+                int blue = (grey & 0x000000FF);
+                grey = (int) ((float) red * 0.3 + (float) green * 0.59 + (float) blue * 0.11);
+//                grey = alpha | (grey << 16) | (grey << 8) | grey;
+
+                pixels[width * i + j] = grey;
+            }
+        }
+        StringBuilder a=new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            a.append("[");
+            for (int j = 0; j < width; j++) {
+                a.append(pixels[width * i + j]).append(",");
+            }
+            a.append("]");
+            Log.i("aaaa", "convertGreyImg: "+a.toString());
+            a=new StringBuilder();
+        }
+        Bitmap result = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        result.setPixels(pixels, 0, width, 0, 0, width, height);
+        return result;
+    }
+
+    public Bitmap gray2Binary(Bitmap graymap) {
+        //得到图形的宽度和长度
+        int width = graymap.getWidth();
+        int height = graymap.getHeight();
+        //创建二值化图像
+        int[] pixels = new int[width * height]; //通过位图的大小创建像素点数组
+
+        graymap.getPixels(pixels, 0, width, 0, 0, width, height);
+        //依次循环，对图像的像素进行处理
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                //得到当前像素的值
+                int col = pixels[width * i + j];
+                //得到alpha通道的值
+                int alpha = col & 0xFF000000;
+                //得到图像的像素RGB的值
+                int red = (col & 0x00FF0000) >> 16;
+                int green = (col & 0x0000FF00) >> 8;
+                int blue = (col & 0x000000FF);
+                // 用公式X = 0.3×R+0.59×G+0.11×B计算出X代替原来的RGB
+                int gray = (int) ((float) red * 0.3 + (float) green * 0.59 + (float) blue * 0.11);
+                //对图像进行二值化处理
+                if (gray <= 95) {
+                    gray = 0;
+                } else {
+                    gray = 255;
+                }
+                // 新的ARGB
+                int newColor = alpha | (gray << 16) | (gray << 8) | gray;
+                //设置新图像的当前像素值
+                pixels[width * i + j] = newColor;
+            }
+        }
+        Bitmap result = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        result.setPixels(pixels, 0, width, 0, 0, width, height);
+        return result;
+    }
+}
